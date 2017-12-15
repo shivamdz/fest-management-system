d7f4a618aa5d52c9803b4cbdef59c1b7d8ba2b9f
shivamdz <shivampandeyusa15@gmail.com>
2017-12-16 02:37:48 +0530

    Admin portal template

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -2,29 +2,29 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/apple-icon.png')}}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{asset('img/favicon.png')}}">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Admin Panel</title>
+	<title>ADMIN PORTAL</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
 
     <!-- Bootstrap core CSS     -->
-    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
 
     <!-- Animation library for notifications   -->
-    <link href="../../assets/css/animate.min.css" rel="stylesheet"/>
+    <link href="{{asset('css/animate.min.css')}}" rel="stylesheet"/>
 
     <!--  Paper Dashboard core CSS    -->
-    <link href="../../assets/css/paper-dashboard.css" rel="stylesheet"/>
+    <link href="{{asset('css/paper-dashboard.css')}}" rel="stylesheet"/>
 
     <!--  Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
-    <link href="../../assets/css/themify-icons.css" rel="stylesheet">
+    <link href="{{asset('css/themify-icons.css')}}" rel="stylesheet">
 
 </head>
 <body>
@@ -32,12 +32,16 @@
 <div class="wrapper">
 	<div class="sidebar" data-background-color="white" data-active-color="danger">
 
+    <!--
+		Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
+		Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
+	-->
 
     	<div class="sidebar-wrapper">
             <div class="logo">
-              <span class="simple-text">
-                    ADMIN PANEL
-                </span>
+                <a class="simple-text">
+                  ADMIN PANEL
+                </a>
             </div>
 
             <ul class="nav">
@@ -47,12 +51,6 @@
                         <p>Dashboard</p>
                     </a>
                 </li>
-								<li >
-										<a href="dashboard.html">
-												<i class="ti-pie-chart"></i>
-												<p>Fest Setup</p>
-										</a>
-								</li>
 
             </ul>
     	</div>
@@ -61,10 +59,39 @@
     <div class="main-panel">
 		<nav class="navbar navbar-default">
             <div class="container-fluid">
-
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar bar1"></span>
+                        <span class="icon-bar bar2"></span>
+                        <span class="icon-bar bar3"></span>
+                    </button>
+                    <!-- <a class="navbar-brand" href="#">ADMIN</a> -->
+                </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                						<li>
+                        <!-- <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="ti-panel"></i>
+								<p>Stats</p>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="ti-bell"></i>
+                                    <p class="notification">5</p>
+									<p>Notifications</p>
+									<b class="caret"></b>
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a href="#">Notification 1</a></li>
+                                <li><a href="#">Notification 2</a></li>
+                                <li><a href="#">Notification 3</a></li>
+                                <li><a href="#">Notification 4</a></li>
+                                <li><a href="#">Another notification</a></li>
+                              </ul>
+                        </li> -->
+						<li>
                             <a href="#">
 								<i class="ti-arrow-circle-right"></i>
 								<p>Logout</p>
@@ -88,10 +115,18 @@
 
         <footer class="footer">
             <div class="container-fluid">
+                <nav class="pull-left">
+                    <!-- <ul>
 
-
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                    </ul> -->
+                </nav>
 				<div class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i><a href="https://www.github.com">Github</a>
+                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by <a href="http://www.github.com">Github</a>
                 </div>
             </div>
         </footer>
@@ -103,21 +138,23 @@
 </body>
 
     <!--   Core JS Files   -->
-    <script src="../../assets/js/jquery-1.10.2.js" type="text/javascript"></script>
-	<script src="../../assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="{{asset('js/jquery-1.10.2.js')}}" type="text/javascript"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
 
 	<!--  Checkbox, Radio & Switch Plugins -->
-	<script src="../../assets/js/bootstrap-checkbox-radio.js"></script>
+	<!-- <script src="{{asset('js/bootstrap-checkbox-radio.js')}}"></script> -->
 
 	<!--  Charts Plugin -->
-	<script src="../../assets/js/chartist.min.js"></script>
+	<!-- <script src="{{asset('js/chartist.min.js')}}"></script> -->
 
     <!--  Notifications Plugin    -->
-    <script src="../../assets/js/bootstrap-notify.js"></script>
+    <!-- <script src="{{asset('js/bootstrap-notify.js')}}"></script> -->
 
-    <!-- Paper Dashboard Core javascript  -->
-	<script src="../../assets/js/paper-dashboard.js"></script>
+    <!--  Google Maps Plugin    -->
+    <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script> -->
 
+    <!-- Paper Dashboard Core javascript and methods for Demo purpose -->
+	<script src="{{asset('js/paper-dashboard.js')}}"></script>
 
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/admin/template');
 });
 Route::get('/admin',function() {
   return view ('/admin/template');
